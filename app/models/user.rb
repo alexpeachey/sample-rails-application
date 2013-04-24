2506c12a4d2fcf8176fea951f67ca7cc12dbbc8e
alexpeachey <alex.peachey@originate.com>
2013-04-24 16:50:33 -0700

    Add slug to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@
 #  email      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  slug       :string(255)
 #
 
 class User < ActiveRecord::Base
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412212906) do
+ActiveRecord::Schema.define(:version => 20130424234950) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(:version => 20130412212906) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
+
+  add_index "users", ["slug"], :name => "index_users_on_slug", :unique => true
 
 end
