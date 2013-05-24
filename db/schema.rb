fe6a377d69801848be685edd8eb25fe743a0aa5b
alexpeachey <alex.peachey@originate.com>
2013-05-24 14:09:13 -0700

    Migrating new flag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130424234950) do
+ActiveRecord::Schema.define(:version => 20130524205910) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(:version => 20130424234950) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.string   "slug"
+    t.boolean  "admin",      :default => false
   end
 
   add_index "users", ["slug"], :name => "index_users_on_slug", :unique => true
